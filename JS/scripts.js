b73dbb8caa0d55a3dabc303b16d9f96858e07e2c
Grzegorz Izydorek <izydorek.g@gmail.com>
2019-03-29 10:32:55 +0100

    first

---
+++ JS/scripts.js
@@ -0,0 +1,36 @@
+function czasDoWydarzenia(rok, miesiac, dzien, godzina, minuta, sekunda, milisekunda)
+{
+	var aktualnyCzas = new Date();
+	var dataWydarzenia = new Date(rok, miesiac, dzien, godzina, minuta, sekunda, milisekunda);
+	var pozostalyCzas = dataWydarzenia.getTime() - aktualnyCzas.getTime();
+	
+	if (pozostalyCzas > 0)
+	{						
+		var s = pozostalyCzas / 1000;	// sekundy
+		var min = s / 60;				// minuty
+		var h = min / 60;				// godziny
+        var d = h / 24;                 // dni
+    
+		var sLeft = Math.floor(s % 60);	        // pozostało sekund		
+		var minLeft = Math.floor(min % 60);	    // pozostało minut
+		var hLeft = Math.floor(h % 24);			// pozostało godzin	
+		var dLeft = Math.floor(d);              //pozostało dni
+    
+		if (minLeft < 10)
+		  minLeft = "0" + minLeft;
+		if (sLeft < 10)
+		  sLeft = "0" + sLeft;
+		
+		return dLeft + " : " + hLeft + " : " + minLeft + " : " + sLeft;
+	}
+  
+	else
+		return "No i w PIZDU wylądowali :D";
+}
+					
+window.onload = function()
+{
+	idElement = "text";
+	document.getElementById(idElement).innerHTML = czasDoWydarzenia(2019, 04, 13, 19, 0, 0, 0);
+	setInterval("document.getElementById(idElement).innerHTML = czasDoWydarzenia(2019, 04, 13, 19, 0, 0, 0)", 1000);
+};
